dcee67cd4721d3f8aef6d94265845a07c86c85de
kokiheey <rivilttm@gmail.com>
2025-06-05 15:49:55 +0200

    Implement Config, Calendar Api Classes Add required packages

---
+++ GoogleCalTracker/ConfigLoader.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace GoogleCalTracker
+{
+    public static class ConfigLoader
+    {
+        public static Config config;
+        private static string path = "config.json";
+        public static Config GetConfig()
+        {
+            if (config != null) return config;
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(path, optional:true, reloadOnChange: true);
+
+            IConfiguration configuration = builder.Build();
+
+            config = configuration.GetSection("Config").Get<Config>() ?? new Config();
+            return config;
+        }
+
+        public static void SaveConfig()
+        {
+            var json = JsonSerializer.Serialize(config, new JsonSerializerOptions { WriteIndented = true});
+            File.WriteAllText(path, json);
+        }
+    }
+}
---
+++ GoogleCalTracker/GoogleCalendarApi.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+
+namespace GoogleCalTracker
+{
+    public static class GoogleCalendarApi
+    {
+        public static async Task<Colors> GetAvailableColorsAsync()
+        {
+            UserCredential credential;
+
+            using (var stream = new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    new[] { CalendarService.Scope.CalendarReadonly },
+                    "user",
+                    CancellationToken.None);
+            }
+
+            var service = new CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "Google Calendar Tracker",
+            });
+
+            Colors colors = await service.Colors.Get().ExecuteAsync();
+            return colors;
+        }
+
+        public static async Task CreateEvent(EventType eventType, DateTime startTime, DateTime endTime)
+        {
+            UserCredential credential;
+
+            using (var stream = new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    new[] { CalendarService.Scope.CalendarReadonly },
+                    "user",
+                    CancellationToken.None);
+            }
+
+            var service = new CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "Google Calendar Tracker",
+            });
+
+            var newEvent = new Event()
+            {
+                Summary = eventType.Name,
+                Start = new EventDateTime()
+                {
+                    DateTime = startTime,
+                    TimeZone = 
+                }
+            };
+        }
+
+    }
+}
